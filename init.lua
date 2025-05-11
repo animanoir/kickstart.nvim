c9beed89a8ff8a5727d91748b7cc7e4a3bb5d685
oscaramontiel <etherealflux94@gmail.com>
2025-05-11 13:57:22 -0600

    adds GHCopilot to plugins

--- init.lua
+++ init.lua
@@ -245,6 +245,7 @@ require('lazy').setup({
     config = true,
     dependencies = { 'nvim-treesitter/nvim-treesitter' },
   },
+  'github/copilot.vim',
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
   -- keys can be used to configure plugin behavior/loading/etc.
